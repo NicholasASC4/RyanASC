396a03b3a12c92c89b327ef4b885477ae36bf223
RyanASC4 <rtaylor@victorycollegiate.org>
2017-07-11 13:01:54 -0400

    first commit

---
+++ pokemonvariable/pokemon-variable.js
@@ -0,0 +1,11 @@
+var dragoniteHP = 91
+var dragoniteNum = 149
+var dragoniteType = "Dragon and Flying"
+var dragoniteBestMove = "Outrage"
+var dragoniteLastEvo = true;
+var dragoniteAttack = 134
+var dragoniteDefense = 95
+var dragoniteSpAttack = 100
+var dragoniteSpDefense = 100
+var dragoniteSpeed = 80
+console.log(dragoniteHP) 
